dd17edc13e46ef4d7674e8f411b7e666d80d5497
carlosrelter <reltercontato@gmail.com>
2023-05-03 21:41:06 -0300

    ngIf

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -8,26 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos = [
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Angular',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'passo informações para componente filho',
-      autoria: 'componente pai',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'minha propriedade é decorada com @Input',
-      autoria: 'componente filho',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'teste aleatorio',
-      autoria: 'Angular',
-      modelo: 'modelo1'
-    },
+    // {
+    //   conteudo: 'Comunicação entre componentes',
+    //   autoria: 'Angular',
+    //   modelo: 'modelo3'
+    // },
+    // {
+    //   conteudo: 'passo informações para componente filho',
+    //   autoria: 'componente pai',
+    //   modelo: 'modelo2'
+    // },
+    // {
+    //   conteudo: 'minha propriedade é decorada com @Input',
+    //   autoria: 'componente filho',
+    //   modelo: 'modelo3'
+    // },
+    // {
+    //   conteudo: 'teste aleatorio',
+    //   autoria: 'Angular',
+    //   modelo: 'modelo1'
+    // },
   ];
 
   constructor() { }
