f279d38d8620fa64d86430a1e7e0ff64a7301869
carlosrelter <reltercontato@gmail.com>
2023-05-05 21:51:15 -0300

    editando criar pensamento com observabl e router

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,22 +11,27 @@ import { Pensamento } from '../pensamento';
 export class CriarPensamentoComponent implements OnInit {
 
   pensamento:Pensamento= {
-    id: 1,
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev front',
+    conteudo: '',
+    autoria: '',
     modelo: 'modelo1'
   }
-  constructor() { }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
 
   criarPensamento(){
-    console.log("Pensamento criado")
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
   }
 
   cancelar(){
-    console.log("cancelado")
+    this.router.navigate(['/listarPensamento'])
   }
 
 }
--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -10,9 +12,13 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento [] = [];
 
-  constructor() { }
+  constructor(private service: PensamentoService ) { }
 
   ngOnInit(): void {
-  }
+    this.service.listar().subscribe((listaPensamentos)=>{
+      this.listaPensamentos = listaPensamentos;
+    })//a inscrição emite notificações toda vez que houver mudança
+  }//é ciclo de vida do componente tudo que sera carregado quando componente for carregado
+
 
 }
